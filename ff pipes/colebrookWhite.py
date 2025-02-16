731b3649fe968385f86cb151255d22a8b97ad26d
kkekevin <kke_kevin@hotmail.com>
2025-02-16 12:33:46 -0300

    Add another method to get ff

--- ff pipes/colebrookWhite.py
+++ ff pipes/colebrookWhite.py
@@ -11,6 +11,13 @@ def y(f):
     return (1 / (sqrt(f))) + 2 * log((eps/(3.7*D)) + (2.51 / (x * sqrt(f))), 10)
 
 # Serghides eq
+def ffS (Re):
+    A = -2 * log10((eps/D) / 3.7 + (12 / Re))
+    B = -2 * log10((eps/D) / 3.7 + ((2.51 * A) / Re))
+    C = -2 * log10((eps/D) / 3.7 + ((2.51 * B) / Re))
+    
+    return (A - ((B - A)**2 / (A - (2 * B) + C)))**-2
+
 # characteristics of 5 tube
 c_tube = ["carbon steel 16.8mm", "acrylic pipe 12mm", "pvc 17mm", "carbon steel 22.4mm", "carbon steel 28.4mm"]
 epss = [0.00004572, 0.000002, 0.0000015, 0.00004572, 0.00004572]
@@ -25,14 +32,33 @@ for tube in c_tube:
     ff = []
     D = Diameter[i]
     eps = epss[i]
-    print(tube)
     for x in Re:
         ff.append(getattr(root(y,0.002), 'x').item())
     plt.plot(Re, np.c_[ff, ff_exp], label=['colebrook ff', 'experimental ff'])
     plt.legend()
     plt.xlabel("Reynolds")
     plt.ylabel("friction factor")
+    plt.title(tube)
     plt.show()
+
+# compare friction factor of carbon steel 16.8mm by two methods
+Re = np.array(pd.read_excel(excel_file, sheet_name='Sheet1', skiprows=row[0], usecols='G', nrows=9))
+ff_exp = np.array(pd.read_excel(excel_file, sheet_name='Sheet1', skiprows=row[0], usecols='H', nrows=9))
+ff = []
+ffE = []
+eps = epss[0]
+D = Diameter[0]
+for x in Re:
+    ff.append(getattr(root(y,0.002), 'x').item())
+    ffE.append(ffS(x))
+
+plt.plot(Re, np.c_[ff, ffE, ff_exp], label=['Colebrook ff', 'Serghides ff', 'exp ff'])
+plt.legend()
+plt.xlabel("Reynolds")
+plt.ylabel("friction factor")
+plt.title(c_tube[0])
+plt.show()
+
 
 
 # Re = [9433.29, 14149.94, 18866.59, 23583.24, 28299.89, 35374.86, 42449.83, 47166.48, 58958.10]
